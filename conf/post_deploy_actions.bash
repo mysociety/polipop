93c3a9149f7c3a42632ea081e4560c218730bb34
Matthew Somerville <matthew@mysociety.org>
2011-10-27 15:04:33 +0100

    Initial commit from django-jumpstart, with names changed as appropriate.

---
+++ conf/post_deploy_actions.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# create/update the virtual environment
+pip install \
+    -q \
+    -E ../polipop-virtualenv \
+    -r requirements.txt
+
+# use the virtualenv just created/updated
+source ../polipop-virtualenv/bin/activate
+
+# make sure that there is no old code (the .py files may have been git deleted) 
+find . -name '*.pyc' -delete
+
+# go to the project directory for local config
+cd ./polipop
+
+# get the database up to speed
+./manage.py syncdb
+./manage.py migrate
+
+# gather all the static files in one place
+./manage.py collectstatic --noinput
+
+cd --
---
+++ polipop/popit/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts  import render_to_response, get_object_or_404, redirect
+from django.template   import RequestContext
+
+# from django.views.generic.list_detail import object_detail, object_list
+
+from popit import models
+
+def home(request):
+    """Homepage"""
+    return render_to_response(
+        'index.html',
+        {},
+        context_instance=RequestContext(request)
+    )
+
